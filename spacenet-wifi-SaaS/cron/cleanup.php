e909714ab6cbaf2e9e826ed834f3d191b959fc72
njiriely <102753395+njiriely@users.noreply.github.com>
2025-10-15 11:53:25 +0300

    Delete spacenet-wifi-SaaS directory

--- spacenet-wifi-SaaS/cron/cleanup.php
+++
@@ -1,9 +0,0 @@
-<?php
-// cron/cleanup.php - System Cleanup Tasks
-require_once '../includes/Database.php';
-require_once '../includes/Session.php';
-
-$session = new Session();
-$expiredSessionsCount = $session->checkExpiredSessions();
-
-echo "Cleaned up {$expiredSessionsCount} expired sessions\n";
